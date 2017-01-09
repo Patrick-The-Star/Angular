5bb32a86b6916d79ec597fd91387a884f2f9803e
Sm0kersneverdie <saugata@metropolia.com>
2017-01-09 10:59:33 +0200

    module 3 completed

---
+++ module3-angular/app.js
@@ -0,0 +1,103 @@
+(function(){
+	'use strict';
+	angular.module('NarrowItDownApp',[])
+	.service('MenuSearchService',MenuService)
+	.controller('NarrowItDownController',narrowFunc)
+	.directive('foundItems',foundItems);
+
+
+	function foundItems(){
+		var ddo={
+			templateUrl: 'display.html',
+			scope:{
+				items:"<",
+				onRemove:"&",
+				empty:"@"
+			},
+			controller:itemFinder,
+			controllerAs:'itemFinder',
+			bindToController:true
+		};
+
+		return ddo;
+	}
+
+	function itemFinder(){
+		var itemFinder = this;
+		itemFinder.onRemove = function(index){
+			itemFinder.items.splice(index,1);
+			console.log("hello");
+		}
+	}
+
+	MenuService.$inject=['$http'];
+	function MenuService($http){
+		var service = this;
+		service.getMatchedMenuItems=function(searchTerm){
+			
+			service.response = $http({
+				method:"GET",
+				url:('https://davids-restaurant.herokuapp.com/menu_items.json')
+			});	
+
+			return service.response;	
+
+			
+		}
+
+		
+
+	}
+
+	narrowFunc.$inject = ['$scope','MenuSearchService'];
+	function narrowFunc($scope,MenuSearchService){
+		var narrower = this;
+
+		$scope.input = "";
+		narrower.empty = "";
+		narrower.term = $scope.input;
+		narrower.promise={};
+		narrower.found=[];
+		narrower.find = function(searchTerm){
+			narrower.found=[];
+			narrower.promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+			narrower.promise.then(function (response){
+				for(var i=0;i<response.data.menu_items.length;i++){
+					
+					
+					if(response.data.menu_items[i].description.indexOf(searchTerm)!==-1){
+						narrower.found.push(response.data.menu_items[i]);
+						
+					}
+				}
+				narrower.empty = "";	
+				
+				if (!/\S/.test(searchTerm)||narrower.found.length==0) {
+    				narrower.found=[];
+    				narrower.empty = "Nothing found";
+					
+				}
+
+			})
+
+			.catch(function(error){
+				console.log(error);
+			});	
+
+
+		
+		}
+
+		narrower.onRemove = function(index){
+			narrower.found.splice(index,1);
+			
+		}
+
+		
+
+		
+
+
+	}
+
+})();
